158b0104260fc77abcb8ec18b5adde79e405d865
casey scott <csnz2018@hotmail.com>
2019-04-01 00:05:57 +0000

    implement emailjs

--- assets/js/scripts/sendMailtest.js
+++
@@ -1,8 +0,0 @@
-sendMail = function(message) {
-    if(message == "SUCCESS") {
-        return "Your message has been sent successfully";
-    }
-    else if(message == "FAILED") {
-        return "Message was not sent";
-    }
-};
