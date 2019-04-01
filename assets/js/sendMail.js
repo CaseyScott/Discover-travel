f394169c94605b9e2bfe826b1fc384cb546f7928
casey scott <csnz2018@hotmail.com>
2019-04-01 05:18:20 +0000

    email sent alert

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,17 +1,19 @@
-function sendMail(contactForm) {
+function sendMail(form) {
     emailjs.send("milestone2", "milestone2", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+        "from_name": form.name.value,
+        "from_email": form.emailaddress.value,
+        "project_request": form.projectsummary.value
     })
     .then(
         function(response) {
             console.log("SUCCESS", response);
-           
+            alert("Your message has been sent successfully");
+            document.getElementById('form').reset();
         },
         function(error) {
             console.log("FAILED", error);
-            
+            alert("Message was not sent");
+            document.getElementById('form').reset();
         }
     );
     return false;  // To block from loading a new page
